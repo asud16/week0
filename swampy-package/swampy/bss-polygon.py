0fcd177d28f1b9ce10a09ed6f51e9914fd7104b1
asud16 <asud@mit.edu>
2013-01-24 13:20:27 -0500

    Update swampy-package/swampy/bss-polygon.py

--- swampy-package/swampy/bss-polygon.py
+++ swampy-package/swampy/bss-polygon.py
@@ -3,9 +3,11 @@
 # Name: Aparna Sud
 
 
-from TurtleWorld import *   	
+from TurtleWorld import * 		
 world = TurtleWorld()			
-bob = Turtle()				
+bob = Turtle()		
+
+import math		
 
 def square(Turtle, distance):
 	for i in range(4):
@@ -22,20 +24,34 @@ def polygon(Turtle, distance, n):
 
 #polygon(bob, 100, 5)
 
-def circle(Turtle, radius):
-	circum = 2 * math.pi * radius
+def circle(Turtle, r):
+	circum = 2 * math.pi * r
 	n = 100
 	distance = circum / n
 	polygon(Turtle, distance, n)
 
-#circle(bob, 50)
-#not drawing the circle on the program
+#circle(bob, 5)
+#Refactoring portion: 
 
-def arc(Turtle, radius, theta):
-	theta = angle
-	circle(Turtle, distance, n)
+def polyline(Turtle, distance, n, angle): 
+	for i in range(n):
+		fd(Turtle, distance)
+		lt(Turtle, angle)
 
-arc(bob, 50, 30)
-#not sure how to do this function
+def polygon2(Turtle, distance, n):
+	angle = 360.0/n
+	polyline(Turtle, distance, n, angle)
+
+def arc(Turtle, r, angle):
+	arc_distance = 2 * math.pi * r * angle/360
+	n = int(arc_distance/3) + 1 # why do this?
+	step_distance = arc_distance/n
+	step_angle = float(angle)/n
+	polyline(Turtle, step_distance, n, step_angle)
+
+def circle2(Turtle, r):
+	arc(Turtle, r, 360)
+
+circle2(bob, 10)
 
 wait_for_user()					
